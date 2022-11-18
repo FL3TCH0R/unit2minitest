eabb69ace2624928c672f1f7da0f260321e84c0f
FL3TCH0R <114429899+FL3TCH0R@users.noreply.github.com>
2022-11-18 13:18:41 -0500

    Add files via upload

---
+++ Question2minitest.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.question2minitest;
+
+/**
+ *
+ * @author fletc
+ */
+public class Question2minitest {
+
+    public static void main(String[] args) {
+        
+        
+        
+    int base = 9;
+    int height = 18; 
+
+
+ double hypotenuse = Math.sqrt(base*base+height*height);
+ 
+ System.out.println(hypotenuse);
+
+        
+        
+        
+        
+        
+    }
+}
